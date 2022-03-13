56da1967a994665020da6787b334da52ec8b56b2
Michael <michael.wedd@gmail.com>
2022-03-12 17:59:19 -0700

    add restful request handler pattern

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -7,24 +7,21 @@ import (
 	"github.com/mgwedd/go-microservices/product-api/data"
 )
 
-type Product struct {
-	logger *log.Logger
-}
-
 type Products struct {
 	logger *log.Logger
 }
 
-func NewProducts(logger *log.Logger) *Product {
-	return &Product{logger}
+func NewProducts(logger *log.Logger) *Products {
+	return &Products{logger}
 }
 
-func (products *Product) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
-	productsList := data.GetProducts()
-	err := productsList.ToJson(responseWriter)
-	if err != nil {
-		http.Error(responseWriter, "Unable to marshal JSON", http.StatusInternalServerError)
+func (products *Products) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+		products.getProducts(responseWriter, request)
+		return
 	}
+
+	responseWriter.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (products *Products) getProducts(responseWriter http.ResponseWriter, request *http.Request) {
